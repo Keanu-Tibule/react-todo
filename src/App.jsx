f80ecaf3c638d271a1d64cc53e29f1f59838299d
Keanu Tibule <keanudane@yahoo.com>
2024-03-26 15:20:08 +0800

    created form task

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+//Custom Components
+import CustomForm from './components/CustomForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const addTask = (task) => {
+    console.log(task);
+  }
 
   return (
     <>
-      <div className="card">
-
+      <div className="container">
+        <header>
+          <h1>Adventurer's Quest List</h1>
+        </header>
+        <CustomForm addTask={addTask}/>
       </div>
     </>
   )
--- src/components/CustomForm.jsx
+++ src/components/CustomForm.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import { useState } from 'react';
+//Library Imports
+import { RocketLaunchIcon } from '@heroicons/react/24/solid'
 
-export const CustomForm = () => {
-  return (
-    <div>CustomForm</div>
-  )
+const CustomForm = ({addTask}) => {
+    const[task,setTask]= useState("");
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        addTask({
+            name: task,
+            checked: false,
+            id: Date.now()
+        });
+        setTask("");
+    }
+
+    return (
+        <form 
+            className="todo"
+            onSubmit={handleFormSubmit}
+            >
+            <div className="wrapper">
+                <input 
+                    type="text"
+                    id="task"
+                    className="input"
+                    value={task}
+                    onInput={(e) => setTask(e.target.value)}
+                    required
+                    autoFocus
+                    maxLength={60}
+                    placeholder="Add Quest"
+                />
+                <label 
+                    htmlFor="task"
+                    className="label"
+                >Add Quest</label>
+            </div>
+            <button 
+                className="btn"
+                aria-label="Add Quest"
+                type="submit"
+                >
+                <RocketLaunchIcon/>
+            </button>
+        </form>
+    )
 }
+export default CustomForm
